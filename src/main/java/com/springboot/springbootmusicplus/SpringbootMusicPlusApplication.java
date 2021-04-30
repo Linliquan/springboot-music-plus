6ad38e1e505dbfdf0a40e9902639a63e29b565c6
linliquan <liquan_101@163.com>
2021-04-30 14:29:31 +0800

    â项目初始化

---
+++ src/main/java/com/springboot/springbootmusicplus/SpringbootMusicPlusApplication.java
@@ -0,0 +1,13 @@
+package com.springboot.springbootmusicplus;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootMusicPlusApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootMusicPlusApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/springboot/springbootmusicplus/SpringbootMusicPlusApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springboot.springbootmusicplus;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootMusicPlusApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
