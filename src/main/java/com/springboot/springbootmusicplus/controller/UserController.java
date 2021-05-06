2b7a5242f2c25a19edc47f224b1844717cec6ea5
linliquan <liquan_101@163.com>
2021-05-06 10:39:41 +0800

    添加lombok插件

--- src/main/java/com/springboot/springbootmusicplus/controller/UserController.java
+++ src/main/java/com/springboot/springbootmusicplus/controller/UserController.java
@@ -2,15 +2,20 @@ package com.springboot.springbootmusicplus.controller;
 
 import com.springboot.springbootmusicplus.entity.User;
 import com.springboot.springbootmusicplus.service.impl.UserService;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * @author linliquan
  * @description:
  * @create 2021/4/30 15:55
  */
+@Slf4j
 @RestController
 @RequestMapping("/user")
 public class UserController {
@@ -22,7 +27,10 @@ public class UserController {
     public String sayHello() {
         System.out.println("Hello world！");
         User user = userServiceImpl.getUserInfoByUserId(10);
-        System.out.println(user);
+        List<User> list = new ArrayList<>();
+        list.add(user);
+        list.forEach(System.out::println);
+        log.info("user:{}",  user);
         return "Hello world！";
     }
 }
--- src/main/java/com/springboot/springbootmusicplus/entity/User.java
+++ src/main/java/com/springboot/springbootmusicplus/entity/User.java
@@ -1,9 +1,14 @@
 package com.springboot.springbootmusicplus.entity;
 
+import lombok.Data;
+
 import java.io.Serializable;
 import java.util.Date;
 
+@Data
 public class User implements Serializable {
+    private static final long serialVersionUID = 6570002271842385911L;
+
     private Integer userId;
 
     private String userName;
@@ -12,81 +17,4 @@ public class User implements Serializable {
 
     private Date createTime;
 
-    private static final long serialVersionUID = 1L;
-
-    public Integer getUserId() {
-        return userId;
-    }
-
-    public void setUserId(Integer userId) {
-        this.userId = userId;
-    }
-
-    public String getUserName() {
-        return userName;
-    }
-
-    public void setUserName(String userName) {
-        this.userName = userName == null ? null : userName.trim();
-    }
-
-    public String getUserPassword() {
-        return userPassword;
-    }
-
-    public void setUserPassword(String userPassword) {
-        this.userPassword = userPassword == null ? null : userPassword.trim();
-    }
-
-    public Date getCreateTime() {
-        return createTime;
-    }
-
-    public void setCreateTime(Date createTime) {
-        this.createTime = createTime;
-    }
-
-    @Override
-    public boolean equals(Object that) {
-        if (this == that) {
-            return true;
-        }
-        if (that == null) {
-            return false;
-        }
-        if (getClass() != that.getClass()) {
-            return false;
-        }
-        User other = (User) that;
-        return (this.getUserId() == null ? other.getUserId() == null : this.getUserId().equals(other.getUserId()))
-            && (this.getUserName() == null ? other.getUserName() == null : this.getUserName().equals(other.getUserName()))
-            && (this.getUserPassword() == null ? other.getUserPassword() == null : this.getUserPassword().equals(other.getUserPassword()))
-            && (this.getCreateTime() == null ? other.getCreateTime() == null : this.getCreateTime().equals(other.getCreateTime()));
-    }
-
-    @Override
-    public int hashCode() {
-        final int prime = 31;
-        int result = 1;
-        result = prime * result + ((getUserId() == null) ? 0 : getUserId().hashCode());
-        result = prime * result + ((getUserName() == null) ? 0 : getUserName().hashCode());
-        result = prime * result + ((getUserPassword() == null) ? 0 : getUserPassword().hashCode());
-        result = prime * result + ((getCreateTime() == null) ? 0 : getCreateTime().hashCode());
-        return result;
-    }
-
-    @Override
-    public String toString() {
-        StringBuilder sb = new StringBuilder();
-        sb.append(getClass().getSimpleName());
-        sb.append(" [");
-        sb.append("Hash = ").append(hashCode());
-        sb.append(", userId=").append(userId);
-        sb.append(", userName=").append(userName);
-        sb.append(", userPassword=").append(userPassword);
-        sb.append(", createTime=").append(createTime);
-        sb.append(", serialVersionUID=").append(serialVersionUID);
-        sb.append("]");
-        return sb.toString();
-    }
 }
