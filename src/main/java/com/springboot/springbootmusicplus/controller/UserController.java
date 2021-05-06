57d9a25c37c7a303392f1d696533dd4e58b63106
linliquan <1511387837@qq.com>
2021-05-06 14:15:46 +0800

    修改请求方法

--- src/main/java/com/springboot/springbootmusicplus/controller/UserController.java
+++ src/main/java/com/springboot/springbootmusicplus/controller/UserController.java
@@ -7,6 +7,7 @@ import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -27,7 +28,7 @@ public class UserController {
     @Autowired
     private UserService userServiceImpl;
 
-    @RequestMapping("/hello")
+    @PostMapping("/hello")
     @ApiOperation(value = "打印hello", httpMethod = "POST")
     public String sayHello() {
         System.out.println("Hello world！");
